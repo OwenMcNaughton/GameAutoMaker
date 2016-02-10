c87c20f4505c450c9282b5630c2d37344cf9c7c7
Owen McNaughton <owen.mcnaughton@gmail.com>
2016-02-10 00:40:35 +0000

    combining sprite clusters into entities, representing them on canvas

---
+++ src/GameAutoMaker.js
@@ -0,0 +1,70 @@
+function GameAutoMaker(footage, pix_tol, move_tol) {
+  this.footage = footage;
+  this.pix_tol = pix_tol;
+  this.move_tol = move_tol;
+  this.sprite_groups = [];
+  
+  this.colors = GetRandomColors(100);
+}
+
+GameAutoMaker.prototype = {
+  ProcessFrame: function(idx) {
+    var sprite_groups = [];
+    var frame = this.footage[idx];
+    
+    if (frame == undefined) return;
+    
+    var sprites = [];
+    for (var i = 0; i != frame.sprites.length; i++) {
+      sprites.push({s: frame.sprites[i], b: true});
+    }
+    
+    for (var i = 0; i != sprites.length; i++) {
+      if (sprites[i].b) {
+        sprites[i].b = false;
+        
+        var done = false;
+        for (var j = 0; j != sprite_groups.length; j++) {
+          for (var k = 0; k != sprite_groups[j].s.length; k++) {
+            if (SpriteOverlap(sprites[i].s, sprite_groups[j].s[k])) {
+              sprite_groups[j].s.push(sprites[i].s);
+              sprite_groups[j].c = Centroid(sprite_groups[j].s);
+              done = true;
+              break;
+            }
+          }
+          if (done) break;
+        }
+        if (done) continue;
+        
+        var sg = {s: [sprites[i].s], c: Centroid([sprites[i].s])};
+        for (var j = 0; j != sprites.length; j++) {
+          if (i != j && sprites[j].b) {
+            if (SpriteOverlap(sprites[i].s, sprites[j].s)) {
+              sprites[j].b = false;
+              sg.s.push(sprites[j].s);
+              sg.c = Centroid(sg.s);
+            }
+          }
+        }
+        sprite_groups.push(sg);
+      }
+    }
+    
+    for (var i = 0; i != sprite_groups.length; i++) {
+      sprite_groups[i].color = this.colors[i];
+    }
+    
+    for (var i = 0; i != sprite_groups.length; i++) {
+      for (var j = 0; j != this.sprite_groups.length; j++) {
+        if (Dist(sprite_groups[i].c, this.sprite_groups[j].c) < this.move_tol) {
+          this.sprite_groups.splice(j, 1, sprite_groups[i]);
+        } else {
+          this.sprite_groups.push(sprite_groups[i]);
+        }
+      }
+    }
+    
+    this.sprite_groups = sprite_groups;
+  },
+};
---
+++ src/util.js
@@ -0,0 +1,48 @@
+function Centroid(sprites) {
+  var c = {x: 0, y: 0};
+  
+  for (var i = 0; i != sprites.length; i++) {
+    var s = sprites[i];
+    c.x += s.pos.x + s.size/2;
+    c.y += s.pos.y + s.size/2;
+  }
+  
+  c.x /= sprites.length;
+  c.y /= sprites.length;
+  return c;
+}
+
+function GetRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+  
+function RectOverlap(r1, r2) {
+  return !(r2.l > r1.r || r2.r < r1.l || r2.t > r1.b || r2.b < r1.t);
+}
+
+function GetRandomColors(cnt) {
+  var colors = [];
+  for (var i = 0; i != cnt; i++) {
+    colors.push(GetRandomColor());
+  }
+  return colors;
+}
+
+function SpriteOverlap(s1, s2) {
+  var r1 = {l: s1.pos.x - 1, r: s1.pos.x + s1.size + 1,
+            t: s1.pos.y - 1, b: s1.pos.y + s1.size + 1};
+  var r2 = {l: s2.pos.x - 1, r: s2.pos.x + s2.size + 1,
+            t: s2.pos.y - 1, b: s2.pos.y + s2.size + 1};
+  return RectOverlap(r1, r2);
+}
+              
+function Dist(v1, v2){
+  var xd = Math.abs(v1.x - v2.x);
+  var yd = Math.abs(v2.y - v2.y);
+  return xd*xd + yd*yd;
+}
